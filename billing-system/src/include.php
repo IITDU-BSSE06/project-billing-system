b01b469ef6ed0f876f8787e0401f7409480e8df7
peacefrog <tawsif93@gmail.com>
2016-11-17 06:07:21 +0600

    Dynamically generate bill for user using JS used JSON parsing and RegularExpression parsing for generating bill NOT DECORATED

--- billing-system/src/include.php
+++ billing-system/src/include.php
@@ -6,6 +6,7 @@
 <link rel="stylesheet" href="../css/bootstrap/bootstrap-theme.min.css">
 <link rel="stylesheet" href="../font-awesome/css/font-awesome.min.css">
 
+<script type="text/javascript" src="../js/math.min.js"></script>
 <script src="../js/jquery/jquery-3.1.1.min.js" type="text/javascript"></script>
 <script src="../js/jquery/jquery.PrintArea.js" type="text/javascript"></script>
 <script src="../js/bootstrap/bootstrap.min.js" type="text/javascript"></script>
