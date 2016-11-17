fe036958658655fed247f50494116affa61793dc
Fazle Mohammed Tawsif <tawsif93@gmail.com>
2016-11-17 10:09:41 +0600

    Fully working dynamic bill in JS

--- billing-system/src/create_bill.php
+++ billing-system/src/create_bill.php
@@ -20,23 +20,22 @@ if (isset($_SESSION['email'])) {
 <head>
 	<?php include('include.php');?>
 	<title>Create New Bill</title>
-	<script type="text/javascript" src="js/math.min.js"></script>
 
 </head>
 <body>
 <div style="width: 1200px; margin: 0 auto;">
 	<?php
-//	include ('header.php');
+		include ('header.php');
 	?>
 	<div class="row" style="height: 550px;">
-<!--		--><?php //include('home_menu.php') ?>
+		<?php include('home_menu.php') ?>
 		<div class="col-sm-7" style=" border-left: 1px solid #BBCDBC; border-right: 1px solid #BBCDBC; min-height: 550px;">
-			<h2 style="color: #4D574E;">Home</h2>
+			<h2 style="color: #4D574E;">Create New Bill</h2>
 			<p id="bill_placement"></p>
 		</div>
 		<div class="col-sm-3">
 			<?php
-//			include ('show_recent_bills.php');
+						include ('show_recent_bills.php');
 			?>
 		</div>
 	</div>
@@ -74,7 +73,7 @@ if (isset($_SESSION['email'])) {
 		'{"row" : "T/S"}]}';
 
 	var calculationDetails = '( 3 * 5 ) * 7;( 4 * 6 ) * 7;( 4 * 6 ) * 7;3 * 7;3 * 7;( 4 * 6 ) * 7;( 4 * 6 ) * 7;( 4 * 6 ) * 7;3 * 7;( 4 * 6 ) * 7;';
-
+	var columnMeasurements = '2,20,7,9,5,7,10,7';
 
 	var obj = JSON.parse(strJSON);
 	tableCreate();
@@ -94,10 +93,13 @@ if (isset($_SESSION['email'])) {
 		columnCount = Object.keys(obj.columns).length;
 		rowCount = Object.keys(obj.rows).length;
 
+		var measurements = columnMeasurements.split(',');
 		var row = tbl.insertRow(-1);
+
 		for (var i = 0; i < columnCount; i++) {
 			var headerCell = document.createElement("TH");
 			headerCell.innerHTML = obj.columns[i].column;
+			headerCell.setAttribute('width', measurements[i]+'%') ;
 			row.appendChild(headerCell);
 		}
 
@@ -129,12 +131,41 @@ if (isset($_SESSION['email'])) {
 				}, true)
 			}
 		}
-
+		addFinalCalculationRows(tbl);
 		body.appendChild(tbl)
 	}
 
+	function addFinalCalculationRows(tbl) {
+		var row = tbl.insertRow(-1);
+		var cell = row.insertCell(-1);
+		cell.innerHTML= '<strong>Total</strong>';
+		cell = row.insertCell(-1);
+		cell.setAttribute('colspan' , columnCount-2);
+		cell = row.insertCell(-1);
+		cell.id = 'total_taka';
 
-	var calculateTotalAmount = function () {
+		row = tbl.insertRow(-1);
+		cell = row.insertCell(-1);
+		cell.innerHTML= '<strong>Tax</strong>';
+		cell = row.insertCell(-1);
+		cell.setAttribute('colspan' , columnCount-2);
+		cell.style.textAlign='center';
+		cell.innerHTML = '<strong>(10% AIT deduction)</strong>';
+		cell = row.insertCell(-1);
+		cell.id = 'tax_taka';
+
+		row = tbl.insertRow(-1);
+		cell = row.insertCell(-1);
+		cell.innerHTML= '<strong>Net</strong>';
+		cell = row.insertCell(-1);
+		cell.setAttribute('colspan' , columnCount-2);
+		cell.style.textAlign='center';
+		cell.innerHTML = '<strong>(Taka Only)</strong>';
+		cell = row.insertCell(-1);
+		cell.id = 'net_taka';
+	}
+
+	function calculateTotalAmount() {
 		var parsedDetails = calculationDetails.split(';');
 		var table = document.getElementById('bill_table');
 
@@ -158,47 +189,66 @@ if (isset($_SESSION['email'])) {
 			}
 
 			table.rows.item(i).cells.item(columnCount-1).innerHTML = math.eval(values);
+
+			calculate_total_tax_net(table.rows);
 		}
 
-	};
+		function calculate_total_tax_net(rows) {
+			var subTotal=0;
+			var total ;
+			var tax ;
+			var net ;
 
-	var calculate = function () {
-		var parsedDetails = calculationDetails.split(';');
+			for (var i = 1 ; i < rowCount ; i++){
+				if(rows.item(i).cells.item(columnCount-1).innerHTML != '')subTotal = math.chain(subTotal).add(parseFloat(rows.item(i).cells.item(columnCount-1).innerHTML)).done();
+			}
 
-		var table = document.getElementById('bill_table');
+			total = subTotal;
+			tax = math.chain(total).multiply(.1).done();
+			net = total-tax;
 
-		var values = '';
-		for( var i = 1 ; i < rowCount ; i++)
-		{
-			var calculationExpression = parsedDetails[(i-1)].split(' ');
-			var rowCells = table.rows.item(i).cells;
-			for(var j = 0 ; j < calculationExpression.length ; j++){
-				if(!isNaN(calculationExpression[j])){
-					values = values.concat(rowCells.item(parseInt(calculationExpression[j])-1).innerHTML.innerHTML+ ' ');
+			document.getElementById('total_taka').innerHTML= math.format(total,{notation: 'fixed', precision: 2});
+			document.getElementById('tax_taka').innerHTML = math.format(tax, {notation: 'fixed', precision: 2});
+			document.getElementById('net_taka').innerHTML = math.format(net, {notation: 'fixed', precision: 2});
+		}
+
+		function calculate() {
+			var parsedDetails = calculationDetails.split(';');
+
+			var table = document.getElementById('bill_table');
+
+			var values = '';
+			for( var i = 1 ; i < rowCount ; i++)
+			{
+				var calculationExpression = parsedDetails[(i-1)].split(' ');
+				var rowCells = table.rows.item(i).cells;
+				for(var j = 0 ; j < calculationExpression.length ; j++){
+					if(!isNaN(calculationExpression[j])){
+						values = values.concat(rowCells.item(parseInt(calculationExpression[j])-1).innerHTML.innerHTML+ ' ');
+					}
 				}
 			}
+			markActiveButtons();
 		}
-		markActiveButtons();
-	};
 
 
-	function markActiveButtons() {
-		var table = document.getElementById('bill_table');
-		var parsedDetails = calculationDetails.split(';');
+		function markActiveButtons() {
+			var table = document.getElementById('bill_table');
+			var parsedDetails = calculationDetails.split(';');
 
-		for( var i = 1 ; i < rowCount ; i++)
-		{
-			var calculationExpression = parsedDetails[(i-1)].split(' ');
+			for( var i = 1 ; i < rowCount ; i++)
+			{
+				var calculationExpression = parsedDetails[(i-1)].split(' ');
 
-			var rowCells = table.rows.item(i).cells;
-			for(var j = 0 ; j < calculationExpression.length ; j++){
-				if(!isNaN(calculationExpression[j])){
-					var span = rowCells.item(parseInt(calculationExpression[j])-1);
-					span.setAttribute('contentEditable' , 'true');
-					rowCells.item(parseInt(calculationExpression[j])-1).setAttribute('class' , 'success');
+				var rowCells = table.rows.item(i).cells;
+				for(var j = 0 ; j < calculationExpression.length ; j++){
+					if(!isNaN(calculationExpression[j])){
+						var span = rowCells.item(parseInt(calculationExpression[j])-1);
+						span.setAttribute('contentEditable' , 'true');
+						rowCells.item(parseInt(calculationExpression[j])-1).setAttribute('class' , 'success');
+					}
 				}
 			}
-		}
 
-	}
+		}
 </script>
