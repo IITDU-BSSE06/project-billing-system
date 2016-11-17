d7559403ee02f92a71e00feae8a917ff5d06d0c2
JUBU <bsse0614@iit.du.ac.bd>
2016-11-17 16:32:43 +0600

    Contact Information update

---
+++ billing-system/src/contact.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Profile</title>
+    <link href="../css/bootstrap/bootstrap.min.css" rel="stylesheet">
+    <link href="../css/body.css" rel="stylesheet">
+    <link href="../css/bootstrap/bootstrap-theme.min.css" rel="stylesheet">
+    <link href="../css/bootstrap/bootstrap-theme.min.css.map" rel="stylesheet">
+
+    <script src="../js/jquery/jquery-3.1.1.min.js" type="text/javascript"></script>
+    <script src="../js/jquery/jquery.PrintArea.js" type="text/javascript"></script>
+    <script src="../js/bootstrap/bootstrap.min.js" type="text/javascript"></script>
+</head>
+<body>
+<div style="width: 1200px; margin: 0 auto;">
+    <?php
+    include_once ('header.php');
+    ?>
+
+    <table class="table table-bordered">
+        <tr  align="center">
+            <td class="col-sm-6">
+                <img class="img-circle" src="../images/jubair.jpg" alt="Image">
+            </td>
+            <td class="col-sm-6">
+                <p>Name: Md. Jubair Ibna Mostafa</p>
+                <a>bsse0614@iit.du.ac.bd</a>
+            </td>
+        </tr>
+        <tr  align="center">
+            <td class="col-sm-6">
+                <img class="img-circle" src="../images/maruf.jpg" alt="Image">
+            </td>
+            <td class="col-sm-6">
+                <p>Name: Ariful Huq Maruf</p>
+                <a>bsse0621@iit.du.ac.bd</a>
+            </td>
+        </tr>
+        <tr align="center">
+            <td class="col-sm-6">
+                <img class="img-circle" src="../images/tawsif.jpg" alt="Image">
+            </td>
+            <td class="col-sm-6">
+                <p>Name: Fazle Mohammad Tawsif</p>
+                <a>tawsif93@gmail.com</a>
+            </td>
+        </tr>
+        <tr align="center">
+            <td class="col-sm-6">
+                <img class="img-circle" src="../images/fabiha.jpg" alt="Image">
+            </td>
+            <td class="col-sm-6">
+                <p>Name: Syeda Tasnim Fabiha</p>
+                <a>bsse0622@iit.du.ac.com</a>
+            </td>
+        </tr>
+        <tr align="center">
+            <td class="col-sm-6">
+                <img class="img-circle" src="../images/mumu.jpg" alt="Image">
+            </td>
+            <td class="col-sm-6">
+                <p>Name: Rubaiyat Jahan Mumu</p>
+                <a>bsse0620@iit.du.ac.com</a>
+            </td>
+        </tr>
+    </table>
+    <?php include ('footer.php');?>
+</body>
+</html>
